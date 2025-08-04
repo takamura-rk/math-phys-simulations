105df4f34656940a9a9983ad6cb450e6716d4c5d
takamura-rk <20235865@etud.univ-evry.fr>
2025-08-04 23:44:42 +0200

    simplification de rrprime

--- Simu_sys_solaire/planets.py
+++ Simu_sys_solaire/planets.py
@@ -12,37 +12,7 @@ class Planet:
     def rprime(r,M,a):
         return np.sqrt(constantes.G*M*(2/r-1/a))
         
-    def rrprime(self, other_planets):
- 
-    
-        total_fx = 0
-        total_fy = 0
 
-        for other in other_planets:
-            if other is self:
-                continue  # on ignore soi-même
-
-            dx = other.x - self.x
-            dy = other.y - self.y
-            distance = np.sqrt(dx**2 + dy**2)
-
-            if distance == 0:
-                continue  # évite division par zéro
-
-            force = constantes.G * self.weigth * other.weigth/ (distance**2)
-            fx = force * dx / distance
-            fy = force * dy / distance
-
-            total_fx += fx
-            total_fy += fy
-
-        # Accélération = force / masse
-        ax = total_fx / self.mass
-        ay = total_fy / self.mass
-
-        return ax, ay
-
-    
     def calc_F_grav(self,other):
     
         dx = other.x - self.x
@@ -57,6 +27,27 @@ class Planet:
         fy = force * dy / distance
 
         return fx, fy
+
+    def rrprime(self, other_planets):
+ 
+    
+        total_fx = 0
+        total_fy = 0
+
+        for other in other_planets:
+            if other is self:
+                continue  # On ignore soi-même
+
+        fx, fy = self.calc_F_grav(other)
+        total_fx += fx
+        total_fy += fy
+
+    # Accélération = force / masse
+        ax = total_fx / self.mass
+        ay = total_fy / self.mass
+
+        return ax, ay
+
     
 
 Mercure  = Planet("Mercure", 3.3011e23)
