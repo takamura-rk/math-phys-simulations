874b1239c4e7d57bcc799ddd6fba872c91dec14c
takamura-rk <20235865@etud.univ-evry.fr>
2025-08-04 23:33:18 +0200

    Mini projet simulation du système solaire

--- Simu_sys_solaire/planets.py
+++ Simu_sys_solaire/planets.py
@@ -12,5 +12,59 @@ class Planet:
     def rprime(r,M,a):
         return np.sqrt(constantes.G*M*(2/r-1/a))
         
+    def rrprime(self, other_planets):
+ 
     
+        total_fx = 0
+        total_fy = 0
+
+        for other in other_planets:
+            if other is self:
+                continue  # on ignore soi-même
+
+            dx = other.x - self.x
+            dy = other.y - self.y
+            distance = np.sqrt(dx**2 + dy**2)
+
+            if distance == 0:
+                continue  # évite division par zéro
+
+            force = constantes.G * self.weigth * other.weigth/ (distance**2)
+            fx = force * dx / distance
+            fy = force * dy / distance
+
+            total_fx += fx
+            total_fy += fy
+
+        # Accélération = force / masse
+        ax = total_fx / self.mass
+        ay = total_fy / self.mass
+
+        return ax, ay
+
     
+    def calc_F_grav(self,other):
+    
+        dx = other.x - self.x
+        dy = other.y - self.y
+        distance = np.sqrt(dx**2 + dy**2)
+        if distance == 0:
+            return (0, 0)  
+
+        force = constantes.G * self.weigth* other.weigth / (distance**2)
+    # Vecteur unitaire dans la direction de 'other'
+        fx = force * dx / distance
+        fy = force * dy / distance
+
+        return fx, fy
+    
+
+Mercure  = Planet("Mercure", 3.3011e23)
+Venus    = Planet("Vénus",   4.8675e24)
+Terre    = Planet("Terre",   5.9724e24)
+Mars     = Planet("Mars",    6.4171e23)
+Jupiter  = Planet("Jupiter", 1.8982e27)
+Saturne  = Planet("Saturne", 5.6834e26)
+Uranus   = Planet("Uranus",  8.6810e25)
+Neptune  = Planet("Neptune", 1.0241e26)
+
